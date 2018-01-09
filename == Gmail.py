4b66d09f7e592dd8149963f7ef75d2c54d11af78
AlecGoldstein123 <34690976+AlecGoldstein123@users.noreply.github.com>
2018-01-09 14:39:49 -0500

    Add files via upload

---
+++ == Gmail.py
@@ -0,0 +1,9 @@
+#2==2 is a true or faulse answer. 2=2 error. != is does not equal to
+
+print("Welcome to gemail")
+myUsername= input("Enter your username and then press return")
+if myUsername== "johndoe":
+    print("Username Successful")
+else:
+    print("Username incorrect")
+
---
+++ GmailPassword.py
@@ -0,0 +1,12 @@
+print("Welcome to Gmail")
+myUsername =input("Enter your username, then press Return: ")
+if myUsername=="johndoe" :
+    print("Username succsesful")
+    myPassword=input("Enter your password, then press Return: ")
+    if myPassword=="sof123@":
+        print("Success")
+    else:
+        print("Unable to log in. Incorrect password")
+else:
+    print("Incorrect. Try again")
+            
