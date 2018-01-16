632d7d9186d607116112959eb25b4c68d285356c
AlecGoldstein123 <34690976+AlecGoldstein123@users.noreply.github.com>
2018-01-16 13:40:43 -0500

    Add files via upload

---
+++ For loop math.py
@@ -0,0 +1,6 @@
+for number in range(51):
+        if number%2==0:
+            print(number, "Even number")
+        elif number %2==1:
+            print(number, "Odd number")
+        
---
+++ Math.py
@@ -0,0 +1,23 @@
+"""#from math import sqrt
+x= sqrt(2.0)
+y= 2.0
+if x*x==y:
+    print (x)
+else:
+    print (y)"""
+
+from math import sqrt
+
+number= float(input("Type in number you want to square root: "))
+number_squared= sqrt(number)
+print(number_squared)
+
+
+
+"""n=1
+m=-1
+if n<-m:
+    print(n)
+else:
+    print(m)"""
+
